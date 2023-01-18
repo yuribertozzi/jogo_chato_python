475e5f2b9bd1a35447493de75319bac4339b200a
Yuri Silva <121751241+yuribertozzi@users.noreply.github.com>
2023-01-18 19:33:27 -0300

    Add files via upload
    É a versão que consegui reconstituir sozinho. Só usa as funções que conheço, sem o campeonato cujo placar exigiria o "for _ range". Também o "elif" é desconhecido no momento - não vão vê-los por aqui.
    
    O "pula uma linha" está bem coerente.

---
+++ meunim.py
@@ -0,0 +1,139 @@
+def pcchoose(n,m):
+
+    if n <= m:
+        
+        return n
+
+    else:
+
+        resto = n % (m +1)
+
+        if resto > 0:
+
+            return resto
+
+    return m
+
+#*********
+
+def userchoose(n,m):
+
+    play = 0
+
+    while play == 0:
+
+        print()
+
+        play = int(input("Quantas peças vai tirar? "))
+
+        if play < 0 or play > m or play > n:
+
+            print()
+
+            print("Número inválido!")
+
+            play = 0
+
+        else:
+
+            return play
+
+#*********
+
+def main():
+
+    print()
+
+    n = int(input("Qual o número de peças? "))
+
+    m = int(input("Máximo de peças que pode tirar: ")) 
+
+    pcturn = True
+
+    if n % (m + 1) == 0:
+
+        pcturn = False
+
+        print()
+
+        print("Você começa!")
+
+
+    else:
+
+        print()
+
+        print("O PC começa!")
+
+#***
+
+    while n > 0:
+
+        if pcturn == True:
+
+            play = pcchoose(n,m)
+
+            pcturn = False
+
+            if play == 1:
+
+                print()
+
+                print("O PC removeu uma peça.")
+
+            else:
+
+                print()
+
+                print("O PC removeu", play, "peças.")
+
+#***
+
+        else:
+
+            play = userchoose(n,m)
+
+            pcturn = True
+
+            if play == 1:
+
+                print()
+
+                print("Você removeu uma peça.")
+
+            else:
+                print()
+
+                print("Você removeu", play, "peças.")
+
+            
+        n = n - play
+
+        if n == 1:
+
+            
+
+            print("Resta uma peça no tabuleiro.")
+
+        else:
+
+            
+
+            print("Restam", n, "peças no tabuleiro.")
+
+
+    if pcturn == True:
+
+        print()
+
+        print("Você venceu!")
+
+    else:
+
+        print()
+
+        print("O PC venceu!")
+
+#***
+        
+main()
